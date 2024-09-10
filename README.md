71b7f3591e21af3d38fc0536b11b83b5208d4daf
Janet Blackquill <uhhadd@gmail.com>
2024-09-10 18:26:14 -0400

    chore: update copyright headers

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <!--
-SPDX-FileCopyrightText: 2022 Carson Black <uhhadd@gmail.com>
+SPDX-FileCopyrightText: 2022 Janet Blackquill <uhhadd@gmail.com>
 
 SPDX-License-Identifier: LicenseRef-KDE-Accepted-GPL
 -->
--- src/NGPropertySavingRestoring.h
+++ src/NGPropertySavingRestoring.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 Carson Black <uhhadd@gmail.com>
+// SPDX-FileCopyrightText: 2022 Janet Blackquill <uhhadd@gmail.com>
 //
 // SPDX-License-Identifier: LicenseRef-KDE-Accepted-GPL
 
